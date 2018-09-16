c5f778e798f1523e5f488e1e3d6010e9b5e4255f
Aleksey Belchenkov <u608110@gmail.com>
2018-09-16 07:41:26 +0300

    Add Local Storage

--- js/appes6.js
+++ js/appes6.js
@@ -54,6 +54,55 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+
+    const books = Store.getBooks();
+
+    books.forEach(function (book)  {
+      const ui = new UI;
+
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    
+    books.forEach(function (book, index)  {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 // Event Listeners for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
   e.preventDefault();
@@ -76,6 +125,9 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
     // Add book to list
     ui.addBookToList(book);
 
+    // Add to LS
+    Store.addBook(book);
+
     // Show success
     ui.showAlert('Книга добавлена!', 'success');
 
@@ -92,6 +144,9 @@ document.getElementById('book-list').addEventListener('click', function (e) {
   const ui = new UI();
   // Delete Book
   ui.deleteBook(e.target);
+
+  // Remove Book
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show message
   ui.showAlert('Книга удалена!', 'success');
